58f31a541ff4858c8a8ca0b6c71a290cf83e4e09
KleberSoaresdev <143618629+KleberSoaresdev@users.noreply.github.com>
2023-11-03 11:16:15 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,5 +1,62 @@
-a = [2, 5, 4, 2, 8, 5, 2]
-b = []
-for y in range(6,-1,-1):
-    b.append(a[y])
-print(b)
+class Pessoa:
+    def __init__(self,nome,peso,idade):
+        self.nome=nome
+        self.peso=peso
+        self.idade=idade
+        self.comendo=False
+        self.andando=False
+        self.dormindo=False
+
+    def comer(self,comida):
+        if self.dormindo ==True:
+            print(f"{self.nome} não posso tô dormindo")
+        elif self.andando ==True:
+            print(f"{self.nome} não posso tô andando")
+        elif self.comendo == False:
+            print(f"{self.nome} começou a comer {comida}")
+            self.comendo =True
+        else:
+            print(f"{self.nome} já tô comendo {comida}")
+
+    def parardecomer(self):
+        if self.comendo ==True:
+            print("Parou de comer")
+            self.comendo =False
+        else:
+            print(f"{self.nome} não está comendo")
+
+    def andar(self):
+        if self.dormindo==True:
+            print("Estou domindo")
+        elif self.comendo==True:
+            print("Estou comendo")
+        elif self.andando ==False:
+            print("Comecou a andar")
+            self.andando=True
+        else:
+            print("Já está andando")
+
+    def parardeandar(self):
+        if self.andando==True:
+            print(f"{self.nome} parou de andar")
+            self.andando=False
+        else:
+            print(f"{self.nome} não está andando")
+
+    def dormir(self):
+        if self.comendo ==True:
+            print("Estou comendo")
+        elif self.andando==True:
+            print("Estou andando")
+        elif self.dormindo==False:
+            self.dormindo = True
+            print(f"{self.nome} foi dormir")
+        else:
+            print(f"{self.nome}Já está dormindo")
+
+    def acordar(self):
+        if self.dormindo ==True:
+            print("Acordei")
+            self.dormindo=False
+        else:
+            print("Não estou dormindo")
